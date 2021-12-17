f987873e64da307be8e522d1cacf71624fdd5f99
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2021-12-17 20:45:34 +0100

    Type inference with state

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import ReactDOM from 'react-dom';
+import GuestList from './state/GuestList';
 
 const App = () => {
   return (
     <div>
-      <h1>App</h1>
+      <GuestList />
     </div>
   );
 };
